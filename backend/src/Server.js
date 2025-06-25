38d522ac1e7e898feeb80835826efd5b5da7d7b6
Thirumoorthy369 <mr.thirumoorthys@gmail.com>
2025-06-26 00:06:07 +0530

    Prepared for deployement

--- backend/src/Server.js
+++ backend/src/Server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors"
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -10,14 +11,15 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-
+const __dirname = path.resolve();
 //Middlewares
-app.use(
+if (process.env.NODE_ENV !== "production") {
+  app.use(
     cors({
-    origin: "http://localhost:5173",
-    credentials: true
-})
-);
+      origin: "http://localhost:5173",
+    })
+  );
+}
 app.use(express.json()); //This middleware parse JSON bodies : req.body = { ... }
 app.use(rateLimiter);
 
@@ -28,6 +30,15 @@ app.use(rateLimiter);
 // }) 
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+//it was only done if app is on render.com
 
 
 //Here we using Endpoint
